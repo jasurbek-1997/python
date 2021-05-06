04c511d778b5130b5f56f013ea58b418d32d07b2
Jasurbek <77716033+jasurbek-1997@users.noreply.github.com>
2021-05-06 22:26:40 +0500

    Create lesson 12.py

---
+++ lesson 12.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 19 09:56:27 2021
+
+@author: User
+"""
+
+
+users = ['alisher1983','aziza','yasina', 'umar']
+
+login = input("Yangi login tanlang:" )
+
+if login in users:
+    print('Login band, yangi login tanalng!')
+else:
+    print("Xush kelibsiz!")
